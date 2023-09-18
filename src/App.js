36d793852f3e789c9664b293418d609d20b424c2
Boyana Dimitrova <boyanadimitrova11@gmail.com>
2023-09-18 21:10:51 +0300

    Added React Router. In progress..

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React from "react";
-import AboutUs from "./pages/AboutUs";
-import GlobalStyle from "./components/GlobalStyled";
+import React from 'react';
+import AboutUs from './pages/AboutUs';
+import OurWork from './pages/OurWork';
+import ContactUs from './pages/ContactUs';
+import Nav from './components/Nav';
+import { Route, Routes } from 'react-router-dom';
+
 
 
 function App() {
+
   return (
-    <div>
-      <GlobalStyle />
-      <AboutUs />  
-    
+    <div className="App">
+
+      <Nav />
+
+        <Routes >
+        <Route path="/" element={<AboutUs />} />
+        <Route path="/work" element={<OurWork />} />
+        <Route path="/constact" element={<ContactUs />} />
+           
+        </Routes>
 
     </div>
   );
---
+++ src/pages/ContactUs.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from "styled-components";
+
+const ContactUs = () => {
+    return(
+        <div>
+            <h1> hsdjhsjk</h1>
+        </div>
+    )
+}
+
+
+export default ContactUs;
